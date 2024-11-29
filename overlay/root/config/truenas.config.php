11d8b08db9752000dfa6cd11678d8a7be63de8ca
Daniel Mejia <2473632+damvcoool@users.noreply.github.com>
2024-11-29 17:48:42 -0500

    Update truenas.config.php

--- overlay/root/config/truenas.config.php
+++ overlay/root/config/truenas.config.php
@@ -2,9 +2,9 @@
 $CONFIG = array(
   'one-click-instance' => true,
   'one-click-instance.user-limit' => 100,
-  'memcache.local' => '\OC\Memcache\APCu',
-  'memcache.distributed' => '\OC\Memcache\Redis',
-  'memcache.locking' => '\OC\Memcache\Redis',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'memcache.distributed' => '\\OC\\Memcache\\Redis',
+  'memcache.locking' => '\\OC\\Memcache\\Redis',
   'redis' => array(
     'host' => 'localhost',
   ),
